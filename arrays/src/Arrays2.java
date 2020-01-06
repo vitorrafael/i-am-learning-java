1e312f1fe1e72f964f90a1bec3bdcc7355d3c269
vitorrafael <vrsilveira07@gmail.com>
2020-01-05 22:19:41 -0300

    Classes related to the Arrays lessons

---
+++ arrays/src/Arrays2.java
@@ -0,0 +1,20 @@
+public class Arrays2 {
+
+    public static void main(String[] args) {
+
+        // Creating an array with values already initialized
+        int[] numbers = {1, 2, 3, 4, 5}; // Length = 5
+
+        // Foreach -> Access each item
+        // The variables must be the same type as the array
+        for(int element : numbers) {
+            System.out.println(element);
+        }
+
+        String[] names = {"Vitor", "Eduardo", "Mikhaela", "Gabriela"};
+
+        for(String name : names) {
+            System.out.println(name);
+        }
+    }
+}
