5d3f08b4cdb0aa0c03937fd7db70f59ba0f53e5a
vitorrafael <vrsilveira07@gmail.com>
2020-03-24 19:31:09 -0300

    Add Student entity

---
+++ springboot-essentials/v2/src/main/java/com/vitorrafael/model/AbstractEntity.java
@@ -0,0 +1,37 @@
+package com.vitorrafael.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass // Won't be created at database
+public class AbstractEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractEntity that = (AbstractEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
--- springboot-essentials/v2/src/main/java/com/vitorrafael/model/Student.java
+++ springboot-essentials/v2/src/main/java/com/vitorrafael/model/Student.java
@@ -1,64 +1,16 @@
 package com.vitorrafael.model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import javax.persistence.Entity;
 
-import static java.util.Arrays.asList;
-
-public class Student {
+@Entity // Map Student class to a table
+public class Student extends AbstractEntity {
     private String name;
-    private int id;
 
-    public  static List<Student> studentsList;
-
-    static {
-        studentRepository();
-    }
-
-    public Student() {
-    }
-
-    public Student(String name) {
-        this.name = name;
-    }
-
-    public Student(String name, int id) {
-        this(name);
-        this.id = id;
-    }
-
-    private static void studentRepository() {
-        studentsList = new ArrayList<>(asList(new Student( "Vitor", 1), new Student("Rafaela", 2)));
-    }
-
-    // There must be Getters and Setters
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
