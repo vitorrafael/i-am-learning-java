2e9a7aeea61d63ab1b7c34b95ca965a9d5abf28b
vitorrafael <vrsilveira07@gmail.com>
2020-03-24 17:18:44 -0300

    Create V1 for the SpringBoot Essentials course

---
+++ springboot-essentials/v1/src/main/java/com/vitorrafael/awesome/util/DateUtil.java
@@ -0,0 +1,17 @@
+package com.vitorrafael.awesome.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+// @Component Transform the class into a Spring Component -> @ComponentScan will scan it
+// The following are specializations of the @Component
+// @Repository  Used when working with Data Access Objects
+// @Service Used in service classes
+@Component
+public class DateUtil {
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+}
