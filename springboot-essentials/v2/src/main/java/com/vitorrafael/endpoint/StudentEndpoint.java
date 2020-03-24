ba127cfc2b8b7deb9b510a0ca172db7a051586c4
vitorrafael <vrsilveira07@gmail.com>
2020-03-24 19:30:28 -0300

    Adapt endpoints to use the database

--- springboot-essentials/v2/src/main/java/com/vitorrafael/endpoint/StudentEndpoint.java
+++ springboot-essentials/v2/src/main/java/com/vitorrafael/endpoint/StudentEndpoint.java
@@ -2,69 +2,61 @@ package com.vitorrafael.endpoint;
 
 import com.vitorrafael.error.CustomErrorType;
 import com.vitorrafael.model.Student;
-import com.vitorrafael.util.DateUtil;
+import com.vitorrafael.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static java.util.Arrays.asList;
 
 // Annotation of Spring MVC based that adds automatically the @ResponseBody to all methods
 // @ResponseBody says that everything returned by a method will be JSON
 @RestController
 @RequestMapping("students") // From where the endpoint will be accessed
 public class StudentEndpoint {
-    // Endpoint is the place where the users will access the API
-    private DateUtil dateUtil;
+    private final StudentRepository DAO;
 
-    @Autowired // Will inject the dependencies
-    public StudentEndpoint(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
+    @Autowired // Will do the Dependency Injection
+    public StudentEndpoint(StudentRepository DAO) {
+        this.DAO = DAO;
     }
 
     // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public ResponseEntity<?> listAll() {
-        return new ResponseEntity<>(Student.studentsList, HttpStatus.OK);
+        return new ResponseEntity<>(DAO.findAll(), HttpStatus.OK);
     }
 
     // @RequestMapping(method = RequestMethod.GET, path = "/{id}") // {?} -> ? Will be a parameter
     @GetMapping(path="/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
         // @PathVariable recovers the variable
-        Student student = new Student();
-        student.setId(id);
-        int index = Student.studentsList.indexOf(student);
-        if(index == -1) {
+        Student student = DAO.findById(id).get();
+        if(student == null ) {
             return new ResponseEntity<>(new CustomErrorType("Student not found."), HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(Student.studentsList.get(index), HttpStatus.OK);
+        return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
     //@RequestMapping(method = RequestMethod.POST)
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Student student) {
         // There must be a default constructor to use a POST request
-        Student.studentsList.add(student);
-        return new ResponseEntity<>(student, HttpStatus.OK);
+        return new ResponseEntity<>(DAO.save(student), HttpStatus.OK);
     }
 
     // @RequestMapping(method = RequestMethod.DELETE)
-   @DeleteMapping
-    public ResponseEntity<?> delete(@RequestBody Student student) {
-        Student.studentsList.remove(student);
+   @DeleteMapping(path="/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long  id) {
+        DAO.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     // @RequestMapping(method = RequestMethod.PUT)
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Student student) {
-        Student.studentsList.remove(student);
-        Student.studentsList.add(student);
+        // The update will be automatically done
+        DAO.save(student);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
+
