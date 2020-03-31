59a5e12a1e03d6abc85cb86c632a2856bcab31e5
vitorrafael <vrsilveira07@gmail.com>
2020-03-31 13:04:56 -0300

    Remove unnecessary dependency

--- springboot-essentials/v2/src/main/java/com/vitorrafael/endpoint/StudentEndpoint.java
+++ springboot-essentials/v2/src/main/java/com/vitorrafael/endpoint/StudentEndpoint.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
