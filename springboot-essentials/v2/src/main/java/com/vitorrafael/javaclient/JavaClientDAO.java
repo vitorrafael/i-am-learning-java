6915e01a44567eb9361a796c0da60e58dfd07951
vitorrafael <vrsilveira07@gmail.com>
2020-04-02 12:30:11 -0300

    Add POST method to Spring Client and encapsulate CRUD methods into a DataAccessObject

---
+++ springboot-essentials/v2/src/main/java/com/vitorrafael/javaclient/JavaClientDAO.java
@@ -0,0 +1,68 @@
+package com.vitorrafael.javaclient;
+
+import com.vitorrafael.model.PageableResponse;
+import com.vitorrafael.model.Student;
+import org.apache.coyote.Response;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class JavaClientDAO {
+    private  static RestTemplate restTemplateUser = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v2/protected/students")
+            .basicAuthentication("vitorrafael", "07042001")
+            .build();
+
+     private static RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v2/admin/students")
+            .basicAuthentication("admin", "admin")
+            .build();
+
+    {
+
+    }
+
+    public Student findByIdWithObject(long id) {
+        Student student = restTemplateUser.getForObject("/{id}", Student.class, id);
+        return student;
+    }
+
+    public ResponseEntity<Student> findByIdWithResponseEntity(long id) {
+        ResponseEntity<Student> studentEntity = restTemplateUser.getForEntity("/{id}", Student.class, id);
+        return studentEntity;
+    }
+
+    public List<Student> listAllWithoutPageable() {
+        ResponseEntity<List<Student>> exchange = restTemplateUser.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {});
+        return exchange.getBody();
+    }
+
+    public List<Student> listAllWithPageable() {
+        ResponseEntity<PageableResponse<Student>> exchange = restTemplateUser.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<PageableResponse<Student>>() {});
+        return exchange.getBody().getContent();
+    }
+
+    public Student saveWithResponseEntity(Student student) {
+        ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/", HttpMethod.POST, new HttpEntity<>(student, createJSONHeader()), Student.class);
+        return exchangePost.getBody();
+    }
+
+    public Student saveWithForObject(Student student) {
+        Student studentResponse = restTemplateAdmin.postForObject("/", student, Student.class);
+        return studentResponse;
+    }
+
+    public Student saveWithForEntity(Student student) {
+        ResponseEntity<Student> responseEntity = restTemplateAdmin.postForEntity("/", student, Student.class);
+        return responseEntity.getBody();
+    }
+
+    private HttpHeaders createJSONHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
--- springboot-essentials/v2/src/main/java/com/vitorrafael/javaclient/JavaSpringClientTest.java
+++ springboot-essentials/v2/src/main/java/com/vitorrafael/javaclient/JavaSpringClientTest.java
@@ -1,55 +1,29 @@
 package com.vitorrafael.javaclient;
 
-import com.vitorrafael.model.PageableResponse;
+
 import com.vitorrafael.model.Student;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 public class JavaSpringClientTest {
+
     public static void main(String[] args) {
-        // Create a Rest Template Client
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v2/protected/students")
-                .basicAuthentication("admin", "admin")
-                .build();
+        JavaClientDAO javaClientDAO = new JavaClientDAO();
 
-        // clientGetExamplesWithoutPageable(restTemplate);
-        clientGetWithPageable(restTemplate);
-    }
+        Student newStudent = new Student();
+        newStudent.setName("Rose Freitas");
+        newStudent.setEmail("rosefreitas@yahoo.com.br");
 
-    private static void clientGetWithPageable(RestTemplate restTemplate) {
-         // Even though the exchange object says that the sort is UNSORTED the server is returning the content in the way that is asked
-        /*
-         * TODO: Investigate exchange "sort" issues
-         */
-        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/?sort=id,asc&sort=name,desc", HttpMethod.GET, null, new ParameterizedTypeReference<PageableResponse<Student>>() {});
-        System.out.println(exchange);
-        for(Student student : exchange.getBody()) {
-            System.out.println(student.getName());
+        Student getStudent = javaClientDAO.findByIdWithObject(27);
+        System.out.println(getStudent);
+
+        Student postStudent = javaClientDAO.saveWithForObject(newStudent);
+        System.out.println(postStudent);
+
+        List<Student> students = javaClientDAO.listAllWithPageable();
+
+        for(Student student : students) {
+            System.out.println(student);
         }
-    }
-
-    private static void clientGetWithoutPageable(RestTemplate restTemplate) {
-        // Get the information and insert it into an object
-        Student student = restTemplate.getForObject("/{id}", Student.class, 12);
-        System.out.println(student.toString());
-
-        // Get the information and insert it into a ResponseEntity
-        ResponseEntity<Student> studentEntity = restTemplate.getForEntity("/{id}", Student.class, 12);
-        System.out.println(studentEntity);
-        System.out.println(studentEntity.getBody());
-
-        // Getting many objects and storing them in an object
-        Student[] students = restTemplate.getForObject("/", Student[].class);
-        System.out.println(students);
-
-        // Getting many objects with a ResponseEntity
-        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {});
-        System.out.println(exchange);
     }
 }
